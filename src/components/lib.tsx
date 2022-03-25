e79e5c4d64a3a204de29bba5c403f90d61d6c852
ljm <1240530162@qq.com>
2022-03-25 22:08:56 +0800

    处理me接口loading/error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 export const Row = styled.div<{
   gap?: number | boolean;
   between?: boolean;
@@ -20,3 +22,20 @@ export const Row = styled.div<{
         : undefined};
   }
 `;
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size="large" />
+  </FullPage>
+);
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const FullPageErrorFallBack = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type="danger">{error?.message}</Typography.Text>
+  </FullPage>
+);
--- src/context/auth_context.tsx
+++ src/context/auth_context.tsx
@@ -3,6 +3,8 @@ import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import * as auth from "./../auth_provider";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallBack, FullPageLoading } from "components/lib";
 const AuthContext = React.createContext<
   | {
       user: User | null;
@@ -28,13 +30,27 @@ const bootstrapUser = async () => {
 };
 AuthContext.displayName = "AuthContext";
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+  if (isError) {
+    return <FullPageErrorFallBack error={error} />;
+  }
   return (
     <AuthContext.Provider
       children={children}
